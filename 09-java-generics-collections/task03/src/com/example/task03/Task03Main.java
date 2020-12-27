e8ebbec2688b511d44f70f4e453278024fa53015
Zav-lab <72087524+Zav-lab@users.noreply.github.com>
2020-12-27 19:01:43 +0500

    Стешин Владислав. Задание9.3

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -1,24 +1,45 @@
 package com.example.task03;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Set;
+import java.util.*;
+import java.io.InputStreamReader;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
-
         List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
         }
-
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
         return null;
+        Scanner scanner = new Scanner(new InputStreamReader(inputStream, charset));
+        Map<String, TreeSet<String>> anagrams = new TreeMap<>();
+
+        while (scanner.hasNextLine()) {
+            String current = scanner.nextLine().toLowerCase();
+            if (current.matches("[а-я]+") && current.length() >= 3) {
+                char[] chars = current.toCharArray();
+                Arrays.sort(chars);
+                String sorted = new String(chars);
+
+                if (!anagrams.containsKey(sorted))
+                    anagrams.put(sorted, new TreeSet<>());
+                anagrams.get(sorted).add(current);
+            }
+        }
+
+        List<Set<String>> result = new ArrayList<>();
+        for (Set set : anagrams.values()) {
+            if (set.size() >= 2)
+                result.add(set);
+        }
+        return result;
     }
 }
